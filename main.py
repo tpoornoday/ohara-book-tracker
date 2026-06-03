0c45386d186830c4fd5f6992ddc704ff5a722b1c
Tiruveedi Poornoday <tpoornoday01@gmail.com>
2026-06-04 01:32:54 +0530

    Fix database connection SSL validation

--- main.py
+++ main.py
@@ -47,6 +47,8 @@ def get_db_connection():
         if hostname not in ("localhost", "127.0.0.1"):
             import ssl
             ssl_ctx = ssl.create_default_context()
+            ssl_ctx.check_hostname = False
+            ssl_ctx.verify_mode = ssl.CERT_NONE
             
         import pg8000.dbapi
         return pg8000.dbapi.connect(
